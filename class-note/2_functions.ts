49b3b51122b8429177521a2a7b8960e0f16da9ba
Rowoon <dev.rowoon@gmail.com>
2026-01-02 13:57:10 +0900

    함수에 타입을 정의하는 기본 방법

---
+++ class-note/2_functions.ts
@@ -0,0 +1,17 @@
+// 파라미터의 타입을 정의하는 방식
+// function sum (a: number, b: number) {
+//   return a + b
+// }
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add (): number {
+  return 10;
+}
+
+
+// 함수에 타입을 정의하는 방식
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10 , 20);
