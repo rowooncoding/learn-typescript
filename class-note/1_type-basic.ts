2a1673566774d32e13c2b5bca2046dc22aefeb38
Rowoon <dev.rowoon@gmail.com>
2026-01-02 13:22:14 +0900

    문자, 숫자, 배열 타입 알아보기

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+// var str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자 선언
+let num: number = 10;
+
+// TS 배열 선언
+let arr: Array<number> = [];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk']; // 숫자는 들어가지 못함!
+let items: number[] = []; // 배열을 리터럴로 선언
