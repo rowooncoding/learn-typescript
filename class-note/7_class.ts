5a62131bba91fed380e659d4f55168fe224f7b21
Rowoon <dev.rowoon@gmail.com>
2026-01-07 14:41:07 +0900

    클래스 문법 사용해보기

---
+++ class-note/7_class.ts
@@ -0,0 +1,11 @@
+class Person {
+  private name: string;
+  public age: number;
+  readonly log: string;
+
+  constructor(name: string, age: number, log: string) {
+    this.name = name;
+    this.age = age;
+    this.log = log;
+  }
+}
