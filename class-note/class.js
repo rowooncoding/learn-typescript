d994b87b0e7364b47ba8f8bbdaa3be1c8ffb6f79
Rowoon <dev.rowoon@gmail.com>
2026-01-07 13:48:48 +0900

    간단한 클래스 함수 작성

---
+++ class-note/class.js
@@ -0,0 +1,12 @@
+// ES6 부터 소개된 새로운 문법
+class Person {
+  // 클래스 로직
+
+  // 생성자
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+var seho = new Person('세호', 30); // 인스턴스 생성 후 객체로 반환
