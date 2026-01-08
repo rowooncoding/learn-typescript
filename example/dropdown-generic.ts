17e03bd266019b340f938dd557aab97607662a5a
Rowoon <dev.rowoon@gmail.com>
2026-01-08 12:59:42 +0900

    interface를 사용해서 타입 정의 리펙토링

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,27 @@
-const emails = [
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+const emails: Email[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
+
+const numberOfProducts: ProductNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+// 이 함수에서 value인자는 string일수도 있고 number일수도 있다!
+function createDropdownItem(item: Email | ProductNumber) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -19,8 +30,10 @@ function createDropdownItem(item) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
-emails.forEach(function (email) {
-  const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
-  selectTag.appendChild(item);
-});
+const selectTag = document.querySelector('#email-dropdown');
+if (selectTag) {
+  emails.forEach(function (email) {
+    const item = createDropdownItem(email);
+    selectTag.appendChild(item);
+  });
+}
