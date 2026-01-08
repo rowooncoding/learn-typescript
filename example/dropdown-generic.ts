35ddaa6d275103ee77f8391e74cd04d948589534
Rowoon <dev.rowoon@gmail.com>
2026-01-08 13:11:16 +0900

    제네릭 타입으로 리펙토링

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,27 +1,41 @@
-interface Email {
-  value: string;
+// value마다 인터페이스를 선언하면 너무 번거로워!
+// -> 제네릭으로 인터페이스 선언!
+// -> dropdown에 대한 인터페이스는 모두 필요가 없어짐!
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
 }
 
-const emails: Email[] = [
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+// 제네릭으로 설정
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
 
-const numberOfProducts: ProductNumber[] = [
+// interface TrueFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
 // 이 함수에서 value인자는 string일수도 있고 number일수도 있다!
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
